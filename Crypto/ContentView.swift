2b9345c36ef4a04e714e7948b1f8aecb09a40506
Artem Amaev <artemamaev2@gmail.com>
2024-08-26 09:44:10 +0300

    Initial Commit

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Артём Амаев on 26.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Артём Амаев on 26.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
